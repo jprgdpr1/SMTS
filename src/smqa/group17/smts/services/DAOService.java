10f50be01156cea79245dbde3c44906bb0b1952b
Mohana Krishnan Ramesh <89975296+Mohan6458@users.noreply.github.com>
2023-12-04 14:04:43 +0000

    Display Available Stocks DAOService.java

--- src/smqa/group17/smts/services/DAOService.java
+++ src/smqa/group17/smts/services/DAOService.java
@@ -2,4 +2,16 @@ package smqa.group17.smts.services;
 
 public class DAOService {
 
+  private static void displayAvailableStocks(Connection connection) throws SQLException {
+        String query = "SELECT * FROM stocks";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            System.out.println("Available Stocks:");
+            System.out.println("Stock Symbol\tStock Price");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("symbol") + "\t\t\t" + resultSet.getDouble("price"));
+            }
+        }
+    }
+  
 }
